3c5c9dab6b10f708d76340c33a9e532c7f7bce40
Ahn-Gihwan <ahngh0113@naver.com>
2021-11-26 15:10:33 +0900

    fix: remove anotation

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
-// import React from 'react'
 import App from '../App'
 
 const Router = () => {
