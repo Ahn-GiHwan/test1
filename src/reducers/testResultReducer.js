8fb2eea4cc9359265c0ff8b9f5424ea790b8aa74
kimyerimweb <kimyerimweb@gmail.com>
2021-11-28 19:52:02 +0900

    docs: add test case about empty data

--- src/reducers/testResultReducer.js
+++ src/reducers/testResultReducer.js
@@ -1,15 +1,21 @@
 export const initialState = {
   currentQuestionAndAnswers: [
-    { id: 1, question: '1번 문제', answer: '1번답' },
-    { id: 2, question: '2번 문제', answer: '2번답' },
-    { id: 3, question: '3번 문제', answer: '3번답' },
-    { id: 4, question: '4번 문제', answer: '4번답' },
-    { id: 5, question: '5번 문제', answer: '5번답' },
-    { id: 6, question: '6번 문제', answer: '6번답' },
-    { id: 7, question: '7번 문제', answer: '7번답' },
-    { id: 8, question: '8번 문제', answer: '8번답' },
-    { id: 9, question: '9번 문제', answer: '9번답' },
-    { id: 10, question: '10번 문제', answer: '10번답' }
+    { id: 1, tag: '리액트', question: '', answer: '1번답' },
+    { id: 2, tag: '자바스크립트', question: '2번 문제', answer: '2번답' },
+    { id: 3, tag: '리액트', question: '3번 문제', answer: '3번답' },
+    { id: 4, tag: '리액트', question: '4번 문제', answer: '4번답' },
+    { id: 5, tag: 'CSS', question: '5번 문제', answer: '5번답' },
+    { id: 6, tag: '리액트', question: '6번 문제', answer: '6번답' },
+    { id: 7, tag: '뷰', question: '7번 문제', answer: '7번답' },
+    { id: 8, tag: '데이터베이스', question: '8번 문제', answer: '8번답' },
+    { id: 9, tag: '리액트', question: '9번 문제', answer: '9번답' },
+    {
+      id: 10,
+      tag: '운영체제',
+      question: '10번문제sssssssssssssssssssssdssdsdsdsdsssdsdsdddssssssssss',
+      answer:
+        '10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ10번답asdadsshdldjdasdadSddsdaSSDDSADADASDSDSDADASDㅇㅇ'
+    }
   ]
 }
 
