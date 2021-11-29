e08828abd24381455f66c5c7fa0da6790c84d615
Ahn-Gihwan <ahngh0113@naver.com>
2021-11-29 13:11:33 +0900

    feat: Button Component
    props
    - text
    - color
    - type
    - outline
    - block
    - clickFunc
    
    type이 report일 경우 신고하기 버튼으로 간주하여 default값이 지정된다.

--- src/components/TheButton.jsx
+++ src/components/TheButton.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from 'reactstrap'
 
-const TheButton = ({ text, color, type, clickFunc }) => {
+const TheButton = ({ text, color, type, outline, block, clickFunc }) => {
   const styles = {
     reportBtn: { position: 'fixed', bottom: 20, right: 20 }
   }
   return type === 'report' ? (
-    <Button color="danger" style={styles.reportBtn} onClick={clickFunc}>
+    <Button
+      color="danger"
+      style={styles.reportBtn}
+      // class={type ='report' && 'report'}
+      outline={outline}
+      block={block}
+      onClick={clickFunc}
+    >
       {text}
     </Button>
   ) : (
-    <Button color={color} onClick={clickFunc}>
+    <Button color={color} outline={outline} block={block} onClick={clickFunc}>
       {text}
     </Button>
   )
@@ -29,9 +36,13 @@ TheButton.propTypes = {
     'danger'
   ]),
   type: PropTypes.oneOf(['report', 'click']),
+  outline: PropTypes.bool,
+  block: PropTypes.bool,
   clickFunc: PropTypes.func
 }
 
 TheButton.defaultProps = {
-  type: 'click'
+  type: 'click',
+  outline: false,
+  block: false
 }
