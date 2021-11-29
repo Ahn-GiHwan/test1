5dedfa96c6b815e8fe96145fbfb1adf0ca8a9b3b
imhaeunk <imhaeunk@gmail.com>
2021-11-29 13:23:06 +0900

    Feat: initialize App.jsx
    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { Button } from 'reactstrap'
+
 
 const App = () => {
   return (
     <div className="container">
-      <div>
-        <Button color="primary">App</Button>
-      </div>
     </div>
   )
 }
