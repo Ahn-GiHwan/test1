57c2e1b0a8ec0bf596a09609145d2c9d4135cbb2
imparang <jeongjaeyn@gmail.com>
2021-11-29 13:20:13 +0900

    feat: Append routes to link to Categries, CategoryCard component

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink, Link } from 'react-router-dom'
 
 import App from '../App'
+import Categories from './Categories'
+import Category from './Category'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <NavLink to="/App">App으로 이동</NavLink>
+      <Link to="/categories" component={<Categories />}>
+        테스트하기
+      </Link>
       <Routes>
         <Route path="/App" element={<App />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/categories/:id" element={<Category />} />
       </Routes>
     </BrowserRouter>
   )
