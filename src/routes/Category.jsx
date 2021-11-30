4f32518871549217863afe7db5baa77cec7ecda4
imparang <jeongjaeyn@gmail.com>
2021-11-30 15:19:58 +0900

    feat: Apply to routes path changed

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Category = () => {
-  return <div>hello</div>
+  // TODO 문제 개수를 골라서 모의 면접 시작
+  return (
+    <div>
+      hello
+      <Link to="/interview">가자</Link>
+    </div>
+  )
 }
 
 export default Category
