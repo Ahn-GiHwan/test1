90c9d427c71094dc4f15e8c0ecbb28d99b59ec84
kimyerimweb <kimyerimweb@gmail.com>
2021-12-02 18:20:01 +0900

    feat: apply infinite scrolling hook

--- src/routes/ResultCheck.jsx
+++ src/routes/ResultCheck.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import {
   Container,
@@ -10,16 +10,23 @@ import {
 } from 'reactstrap'
 
 import QuestionView from '../components/QuestionView'
+import Loader from '../components/Loader'
+import { useInterSection } from '../hooks/useInterSection'
 
 const ResultCheck = () => {
   const currentQuestionAndAnswers = useSelector(
     state => state.testResultReducer.currentQuestionAndAnswers
   )
 
+  const [loading, QNAItems, setTarget] = useInterSection(
+    currentQuestionAndAnswers,
+    20
+  )
+
   return (
     <>
       <Container fluid>
-        <ListGroup style={{ marginBottom: 15 }}>
+        <ListGroup style={{ margin: '20px  0' }}>
           <ListGroupItem color="primary">
             <Row>
               <Col>
@@ -30,13 +37,16 @@ const ResultCheck = () => {
               </Col>
             </Row>
           </ListGroupItem>
-          {currentQuestionAndAnswers.map(qna => {
-            return (
-              <ListGroupItem key={qna.id}>
-                <QuestionView qna={qna} />
-              </ListGroupItem>
-            )
-          })}
+          <div style={{ height: '75vh', overflow: 'auto' }}>
+            {QNAItems.map(qna => {
+              return (
+                <ListGroupItem key={qna.id}>
+                  <QuestionView qna={qna} />
+                </ListGroupItem>
+              )
+            })}
+            <div ref={setTarget}>{loading && <Loader />}</div>
+          </div>
         </ListGroup>
         <Button color="primary" style={{ position: 'absolute', right: 10 }}>
           복습 목록에 추가하기
