6f01190f9822b349d66656dfc032db29700e9476
imparang <jeongjaeyn@gmail.com>
2021-12-09 15:34:45 +0900

    feat: Design Main Home page UI layout

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,26 +1,94 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import Categories from './Categories'
-import { Container, Button, Row } from 'reactstrap'
-import '../scss/components/Home.scss'
+import { Container, Button, Row, Col } from 'reactstrap'
 import React, { useCallback } from 'react'
 import TheButton from '../components/TheButton'
 import Login from './Login'
 
-const Home = ({ title, isLogged, LoggedState }) => {
+const Home = ({ isLogged, LoggedState }) => {
   const onLogOut = useCallback(() => {
     LoggedState(false)
   }, [isLogged])
   return (
     <Container>
-      <h1>{title}</h1>
       <Row>
-        <div className="content">
-          <Link to="Home">
-            <button className="ring-button">면접대비 바로 시작해보기!</button>
-          </Link>
-          {/* <Link to="AddQuestion">내 질문 등록하기</Link> */}
-        </div>
-        <Categories />
+        <Col style={{ textAlign: 'center' }}>
+          <div style={{ border: '1px solid red' }}>
+            <h3>Today Hot topic</h3>
+            <span>더 많은 토픽들을 확인하고 싶으세요?</span>
+            <div>
+              <h4>NAVER 필수 면접 문제</h4>
+              <span>우리 회사에 왜 지원하셨어요?</span>
+              <Button
+                style={{
+                  borderRadius: '20px',
+                  backgroundColor: '#c05562',
+                  border: 'none',
+                  fontSize: '14px',
+                  padding: '8px 16px'
+                }}
+              >
+                지금 확인하기
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <hr />
+
+      <Row>
+        <Col>
+          <div style={{ border: '1px solid red' }}>
+            <h3>Recent</h3>
+            <div style={{ textAlign: 'center' }}>
+              <h4>NAVER 필수 면접 문제</h4>
+              <span>12 solved / 25</span>
+              {/* Slide 로 넘기기 */}
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <br />
+
+      <Row>
+        <Col>
+          <div style={{ border: '1px solid red' }}>
+            <h3>All Catogories</h3>
+            <div style={{ textAlign: 'center' }}>
+              <h4>All Random</h4>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-evenly',
+                  alignItems: 'center'
+                }}
+              >
+                <Button
+                  style={{
+                    borderRadius: '20px',
+                    backgroundColor: '#c05562',
+                    border: 'none',
+                    fontSize: '14px',
+                    padding: '8px 16px'
+                  }}
+                >
+                  Start Now
+                </Button>
+                <Button
+                  style={{
+                    borderRadius: '20px',
+                    backgroundColor: '#3186c4',
+                    border: 'none',
+                    fontSize: '14px',
+                    padding: '8px 16px'
+                  }}
+                >
+                  Pick Up
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Col>
       </Row>
     </Container>
   )
